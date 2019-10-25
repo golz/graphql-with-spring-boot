26c0f180c0d77866fc751a694705510cc7072140
Goldwin Japar <goldwin.japar@gdn-commerce.com>
2019-10-25 16:05:26 +0700

    first commit

---
+++ src/main/java/com/blimart/graphql/entity/Person.java
@@ -0,0 +1,37 @@
+package com.blimart.graphql.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author goldwin.japar
+ */
+@Data
+@EqualsAndHashCode
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Person implements Serializable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "job")
+    private String job;
+
+    @Column(name = "salary")
+    private BigDecimal salary;
+}
